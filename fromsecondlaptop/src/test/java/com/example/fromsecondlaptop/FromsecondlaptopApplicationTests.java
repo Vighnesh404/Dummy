2d8c448efed525f4127dd205dc595caae3154030
Dhulipala <2406369@cognizant.com>
2025-06-09 18:21:59 +0530

    a new proj added

---
+++ fromsecondlaptop/src/test/java/com/example/fromsecondlaptop/FromsecondlaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fromsecondlaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromsecondlaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springdemoproject/src/main/java/com/example/springdemoproject/java/helloworld.java
@@ -0,0 +1,7 @@
+package com.example.springdemoproject.java;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+    }
+}
